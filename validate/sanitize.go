8779cc1b8a8a1c0b544369f2b4fcea28511cf92f
diep.dao <diep.dao@idnerd.com>
2025-05-16 15:31:26 +0000

    package test

---
+++ validate/sanitize.go
@@ -0,0 +1,39 @@
+package pkg
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+
+	"github.com/go-playground/validator/v10"
+)
+
+func ValidateStruct(req interface{}) error {
+	check := validator.New()
+	if err := check.Struct(req); err != nil {
+		return err
+	}
+	return nil
+}
+
+func JSON(err error) error {
+	var validationErrs validator.ValidationErrors
+	if errors.As(err, &validationErrs) {
+		missingFields := make(map[string]string)
+		for _, fieldErr := range validationErrs {
+			missingFields[fieldErr.Field()] = "is required"
+		}
+		return errors.New("validation failed: missing fields")
+	}
+
+	var unmarshalTypeError *json.UnmarshalTypeError
+	if errors.As(err, &unmarshalTypeError) {
+		return errors.New(unmarshalTypeError.Field + " should be a " + unmarshalTypeError.Type.String())
+	}
+
+	return err
+}
+
+func Test() {
+	log.Println("test")
+}
