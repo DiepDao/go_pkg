690becd2c19bb2a8c0371e2c66d5402f0a713bea
diep.dao <diep.dao@idnerd.com>
2025-05-27 16:21:57 +0000

    interface{} replace for any

--- validate/sanitize.go
+++ validate/sanitize.go
@@ -12,7 +12,7 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
-func getFieldNames(req interface{}) map[string]bool {
+func getFieldNames(req any) map[string]bool {
 	fields := make(map[string]bool)
 	t := reflect.TypeOf(req)
 	if t.Kind() == reflect.Ptr {
@@ -30,14 +30,14 @@ func getFieldNames(req interface{}) map[string]bool {
 	return fields
 }
 
-func CheckSchema(req interface{}, r *http.Request) error {
+func CheckSchema(req any, r *http.Request) error {
 	bodyBytes, err := io.ReadAll(r.Body)
 	if err != nil {
 		return errors.New("failed to read request body")
 	}
-	r.Body = io.NopCloser(bytes.NewReader(bodyBytes)) // Restore body for reuse
+	r.Body = io.NopCloser(bytes.NewReader(bodyBytes))
 
-	var rawJSON map[string]interface{}
+	var rawJSON map[string]any
 	if err := json.Unmarshal(bodyBytes, &rawJSON); err != nil {
 		return errors.New("invalid JSON format: " + err.Error())
 	}
@@ -52,7 +52,7 @@ func CheckSchema(req interface{}, r *http.Request) error {
 	return json.Unmarshal(bodyBytes, req)
 }
 
-func EnforceSchemaRules(req interface{}) error {
+func EnforceSchemaRules(req any) error {
 	check := validator.New()
 	if err := check.Struct(req); err != nil {
 		return parseError(err)
